654c8dbc45f19bdc6817d05b1fbe57879badb1c9
jaozinhodagarapa <joaovictorribeirooliveira49@gmail.com>
2024-03-12 10:18:23 -0300

    Update

--- Script-ExemploJoins.sql
+++ Script-ExemploJoins.sql
@@ -67,6 +67,80 @@ select frutas_id, cores_id from cores_frutas cf ;
 
 
 
+create table clientes (
+	id int,
+	nome varchar(50),
+	primary key (id)
+);
+
+
+create table pedidos (
+	id int,
+	cliente_id int,
+	data_pedido date,
+	primary key(id),
+	constraint fk_pedidos_clientes
+		foreign key (cliente_id)
+		references clientes(id)
+);
+
+insert into clientes (id,nome) values
+(1, 'João'),
+(2, 'Maria'),
+(3, 'Pedro'),
+(4, 'Juvenal'),
+(5, 'Carlos'),
+(6, 'Marcos');
+
+insert into pedidos (id, cliente_id, data_pedido) values
+(101, 1, '2024-03-01'),
+(102, 1, '2024-03-01'),
+(103, 2, '2024-03-01'),
+(104, 3, '2024-03-01'),
+(105, 3, '2024-03-01'),
+(106, 4, '2024-03-01'),
+(107, 2, '2024-03-01'),
+(108, 5, '2024-03-01'),
+(109, 5, '2024-03-01');
+
+insert into pedidos (id, data_pedido) values (110, '2024-03-11');
+
+select id, nome from clientes;
+select id, cliente_id, data_pedido from pedidos;
+
+
+-/* INNER JOIN: Retorna registros quando há pelo menos uma correspondência em ambas as tabelas */
+
+ select c.id, c.nome, p.id, p.data_pedido, p.cliente_id from clientes c
+ inner join pedidos p on  c.id = p.cliente_id;
+
+/* neste exemplo, estamos selecionando todas as colunas das tabelas (clientes e pedido) onde há uma correspondência entro o id na tabela
+ * e o cliente_id na tabela pedidos */
+
+
+/* LEFT JOIN: Retorna todso os registros da tabela da esquerda (primeira tabela mencionada) e os registros correspondentes da tabela da
+ * direita (segunda tabela mencionada). */
+
+select c.id, c.nome, p.id, p.data_pedido, p.cliente_id from clientes c left join pedidos p on c.id = p.cliente_id;
+
+/*aqui, estamos selecionado todos os registros da tabela clientes e os registros correpondetes da tabela pedidos,se houver, com base
+ * na correspondência de id e cliente_id*/
+
+/* RIGHT JOIN: Retorna todos os registros da tabela da direita (segunda tabela mencionada) e os registros correspondetes da tabela da
+ * esquerda (primeira tabela mencionada).*/
+
+select c.id, c.nome, p.id, p.data_pedido, p.cliente_id from clientes c right join pedidos p on c.id = p.cliente_id;
+
+/* este comando retorna todos os registrosda tabela pedidos e os registros correspondente da tabela clientes, se houver, com base na 
+ * correspondência de id e cliente_id*/
+
+
+
+
+
+
+
+
 
 
 
